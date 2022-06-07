76a7dcf397da96ad762534561a9baa12b2638bb1
Danny Lin <danny@kdrag0n.dev>
2022-06-07 08:51:44 +0300

    blossom: Flatten APEXs for performance
    APEXs have quite a bit of overhead in updatable form: they are ext4
    filesystems mounted using loopback block devices, stored on yet another
    ext4 filesystem (system or vendor). They also increase boot time
    substantially due to needing to set up and mount 27 images with AVB. On
    a Pixel 2 XL, apexd-bootstrap took 485 ms during boot, and apexd took
    471 ms for a total of 955 ms — nearly a second — spent on APEX setup,
    which is quite significant for modern devices. At least 422 ms out of
    that was spent blocking the rest of the system's initialization, but the
    overall slowdown is likely greater due to increased system load.
    
    Updatable APEXs likely also come with a non-negligible amount of memory
    overhead due to setting up and mounting so many filesystem images, as
    well as AVB and potential double-caching effects caused by having two
    layers of filesystems in use.
    
    Being able to update APEXs is useless for us anyway because we don't
    use Google's prebuilt modules and we have no other way to deliver APEX
    updates. Even if we were able to use Google's modules, we still want to
    be able to modify many of the APEX modules, so we need to build them
    from source.
    
    Flattened APEXs have negligible overhead, since they're just stored as
    directories in /<partition>/apex and bind-mounted to /apex/<pkg name>.
    
    Change-Id: I94d671d9b0b161df32f9a5f62a7209e51d22a358
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # API levels
 PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Flatten APEXs for performance
+OVERRIDE_TARGET_FLATTEN_APEX := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
