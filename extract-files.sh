439f23f980b5444d3d6efcae233ec98ad99782d2
SamarV-121 <samarvispute121@pm.me>
2022-09-17 14:45:43 +0100

    blossom: Rename libwifi-hal.so
    * To prevent conflict with AOSP's
    
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>
    Signed-off-by: fukiame <fukiame@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -54,22 +54,11 @@ if [ -z "${SRC}" ]; then
 fi
 
 function blob_fixup() {
-    # case "${1}" in
-    #     vendor/lib/libsample1.so)
-    #         sed -i 's|/data/misc/sample1|/data/misc/sample2|g' "${2}"
-    #         ;;
-    #     vendor/lib64/libsample2.so)
-    #         "${PATCHELF}" --remove-needed "libsample3.so" "${2}"
-    #         "${PATCHELF}" --add-needed "libsample4.so" "${2}"
-    #         ;;
-    #     vendor/lib/libsample5.so)
-    #         "${PATCHELF}" --replace-needed "libsample6.so" "libsample7.so" "${2}"
-    #         ;;
-    #     vendor/lib/libsample7.so)
-    #         "${PATCHELF}" --set-soname "libsample7.so" "${2}"
-    #         ;;
-    # esac
-    :
+    case "${1}" in
+	vendor/lib*/libwifi-hal-mtk.so)
+            "$PATCHELF" --set-soname libwifi-hal-mtk.so "${2}"
+            ;;
+    esac
 }
 
 # Initialize the helper
