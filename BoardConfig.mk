a7ce777402f91bc3b9b51d55d467942ffd5b383a
Ookiineko <chiisaineko@protonmail.com>
2023-07-03 09:52:57 +0800

    blossom: use MediaTek SELinux policies
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,5 +147,8 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 # VNDK
 BOARD_VNDK_VERSION := current
 
+# SELinux
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Inherit the proprietary files
 include vendor/xiaomi/blossom/BoardConfigVendor.mk
