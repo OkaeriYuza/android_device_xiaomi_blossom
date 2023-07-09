1c5640f2969bd51da36007919b64adf548dccdab
Ookiineko <chiisaineko@protonmail.com>
2023-07-09 08:57:34 +0800

    blossom: add keymaster libs and VINTFs
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- device.mk
+++ device.mk
@@ -80,6 +80,17 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@3.0.vendor \
+    android.hardware.keymaster@4.0.vendor \
+    libkeymaster4.vendor \
+    libkeymaster4support.vendor \
+    libkeymaster_messages.vendor \
+    libkeymaster_portable.vendor \
+    libpuresoftkeymasterdevice.vendor \
+    libsoft_attestation_cert.vendor
+
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.0.vendor \
