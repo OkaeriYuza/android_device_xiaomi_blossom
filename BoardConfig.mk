30a2bf5c070416f5c4811a8a170911f3068e892a
Pranav Vashi <neobuddy89@gmail.com>
2023-07-12 19:20:38 +0800

    blossom: use libinit to properly set props for each device
    * reversed from Redmi 9A / POCO C31 / Redmi 10A MIUI
    
    * modified from https://github.com/crdroidandroid/android_device_xiaomi_sm6150-common/commits/13.0/libinit
      and https://github.com/crdroidandroid/android_device_xiaomi_sweet/tree/13.0/libinit
    
    * this should allow RIL to work (or resolve bootloops caused by RIL i guess)
    
    Co-authored-by: Sushrut1101 <guptasushrut@gmail.com>
    Co-authored-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Co-authored-by: Ookiineko <chiisaineko@protonmail.com>
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,6 +39,10 @@ TARGET_NO_BOOTLOADER := true
 # Display
 TARGET_SCREEN_DENSITY := 320
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_xiaomi_blossom
+TARGET_RECOVERY_DEVICE_MODULES := libinit_xiaomi_blossom
+
 # Kernel - prebuilt
 TARGET_KERNEL_CONFIG := blossom_defconfig # no file, only make build system happy
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)-kernel/kernel # automatically copied
