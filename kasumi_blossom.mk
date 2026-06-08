0340ef6d9bdba5bd15768d01c66f20ec9fadfaa2
fukiame <fukiame@proton.me>
2026-06-09 01:08:40 +0700

    blossom: skip backuptool
    Signed-off-by: fukiame <fukiame@proton.me>

--- kasumi_blossom.mk
+++ kasumi_blossom.mk
@@ -22,6 +22,7 @@ KASUMI_INCLUDE_GCGOP := true
 # Kasumi reLive flags
 TARGET_EXCLUDE_ADB_ROOT := true
 TARGET_EXCLUDE_LOS_EXTRA_CLI_UTILS := true
+TARGET_SKIP_BACKUPTOOL := true
 
 PRODUCT_DEVICE := blossom
 PRODUCT_NAME := kasumi_blossom
