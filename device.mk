b85c4004cc1a346cc2cb0a2a42c5ab9368625af5
Ookiineko <chiisaineko@protonmail.com>
2026-08-19 17:04:59 +0700

    fixup! blossom: copy android.software.verified_boot.xml
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
 # Product characteristics
 PRODUCT_CHARACTERISTICS := default
 
