f2cfad50b26f259fa39eb5155318dc2e36955616
fukiame <fukiame@proton.me>
2026-08-26 16:42:04 +0700

    blossom: build android.hardware.bluetooth.audio@2.0-impl
    Signed-off-by: fukiame <fukiame@proton.me>

--- device.mk
+++ device.mk
@@ -45,6 +45,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0 \
     android.hardware.bluetooth.audio@2.1 \
+    android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
